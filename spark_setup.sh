df9c1d437bb45feda8f68486798fd93843ea05ea
nkalband <testuser@soe10-vm9.aus.stglabs.ibm.com>
2017-04-17 02:43:41 -0500

    updated to take hostname for master

--- spark_setup.sh
+++ spark_setup.sh
@@ -12,10 +12,12 @@ fi
 
 log=`pwd`/logs/spark_setup_$current_time.log
 echo -e | tee -a $log
-MASTER=$1
-SLAVES=`echo $2 | tr "," "%"`
+#MASTER=$1
+MASTER=$(ssh $1 "hostname")
+#SLAVES=`echo $2 | tr "," "%"`
+SLAVES=`cat ${HADOOP_HOME}/etc/hadoop/slaves | tr "\n" "%"`
 #Logic to create server list 
-echo $SLAVES | tr "%" "\n" | grep $MASTER &>>/dev/null
+echo $SLAVES | tr "%" "\n" | grep $MASTER 
 if [ $? -eq 0 ]
 then
     #if master is also used as data machine 
@@ -24,10 +26,13 @@ else
     SERVERS=`echo ''$MASTER'%'$SLAVES''`
 fi
 ##backing the hive-site.xml file
-cp ${SPARK_HOME}/conf/hive-site.xml ${WORKDIR}
 
-SPARK_DIR=`ls -ltr ${WORKDIR}/spark-*-SNAPSHOT-bin-hadoop-*.tgz | tail -1 | awk '{print $9}' | cut -c1-50` 2>>/dev/null
-SPARK_FILE=`ls -ltr ${WORKDIR}/spark-*-SNAPSHOT-bin-hadoop-*.tgz | tail -1 | awk '{print $9}'` 2>>/dev/null
+echo "Backing up the config files hive-site.xml and spark-defaults.conf"
+cp ${SPARK_HOME}/conf/hive-site.xml ${WORKDIR}
+cp ${SPARK_HOME}/conf/spark-defaults.conf ${WORKDIR}
+
+SPARK_DIR=`ls -ltr ${WORKDIR}/spark-*-SNAPSHOT-bin-hadoop-*.tgz | tail -1 | awk '{print $9}' | cut -c1-50` 
+SPARK_FILE=`ls -ltr ${WORKDIR}/spark-*-SNAPSHOT-bin-hadoop-*.tgz | tail -1 | awk '{print $9}'` 
 if [ $? -ne 0  ];
 then
     echo "Spark tgz file does not exist. Please rerun the spark validation job to generate again." | tee -a $log
@@ -37,21 +42,21 @@ fi
 
 ## Exporting SPARK_HOME to the PATH and Add scripts to the PATH
 
-for i in `echo $SERVERS |cut -d "=" -f2 | tr "%" "\n" | cut -d "," -f1`
+for i in `echo $SERVERS | tr "%" "\n"`
 do
 
     if [ $i != $MASTER ]
 	then
 	    echo 'Deleting old spark file and copying new Spark setup file on '$i'' | tee -a $log
-		ssh $i "rm ${SPARK_FILE}" &>>/dev/null
+		ssh $i "rm ${SPARK_FILE}" 
 	    scp ${SPARK_FILE} @$i:${WORKDIR} | tee -a $log
 	fi
 	
-	ssh $i '[ -d '${SPARK_DIR}' ]' &>>/dev/null
+	ssh $i '[ -d '${SPARK_DIR}' ]' 
 	if [ $? -eq 0 ]
 		then 
 		echo 'Deleting existing spark folder '$SPARK_DIR'  from '$i' '| tee -a $log
-		ssh $i "rm -rf ${SPARK_DIR}" &>>/dev/null
+		ssh $i "rm -rf ${SPARK_DIR}" 
 	fi
 	
 	echo 'Unzipping Spark setup file on '$i'' | tee -a $log
@@ -63,7 +68,7 @@ do
 	echo "export PATH=\$SPARK_HOME/bin:\$PATH">>tmp_b
 	echo '#StopSparkEnv'>>tmp_b
 		
-	scp tmp_b @$i:${WORKDIR}&>>/dev/null
+	scp tmp_b @$i:${WORKDIR}
 		
 	ssh $i "grep -q "SPARK_HOME" ~/.bashrc"
 	if [ $? -ne 0 ];
@@ -96,40 +101,17 @@ cp ${SPARK_HOME}/conf/slaves.template ${SPARK_HOME}/conf/slaves
 sed -i 's|localhost||g' ${SPARK_HOME}/conf/slaves
 cat ${HADOOP_HOME}/etc/hadoop/slaves>>${SPARK_HOME}/conf/slaves
 
-echo -e "Configuring Spark history server" | tee -a $log
-
-cp $SPARK_HOME/conf/spark-defaults.conf.template $SPARK_HOME/conf/spark-defaults.conf
-grep -q "#StartSparkconf" $SPARK_HOME/conf/spark-defaults.conf 
-if [ $? -ne 0 ];
-then
-    echo "#StartSparkconf" >> $SPARK_HOME/conf/spark-defaults.conf
-    echo "spark.eventLog.enabled   true" >> $SPARK_HOME/conf/spark-defaults.conf
-    echo 'spark.eventLog.dir       '${HOME}'/hdfs_dir/spark-events' >> $SPARK_HOME/conf/spark-defaults.conf 
-    echo "spark.eventLog.compress  true" >> $SPARK_HOME/conf/spark-defaults.conf
-    echo 'spark.history.fs.logDirectory   '${HOME}'/hdfs_dir/spark-events' >> $SPARK_HOME/conf/spark-defaults.conf
-    echo "#StopSparkconf">> $SPARK_HOME/conf/spark-defaults.conf
-else
-    sed -i '/#StartSparkconf/,/#StopSparkconf/ d' $SPARK_HOME/conf/spark-defaults.conf
-    echo "#StartSparkconf" >> $SPARK_HOME/conf/spark-defaults.conf 
-    echo "spark.eventLog.enabled   true" >> $SPARK_HOME/conf/spark-defaults.conf
-    echo 'spark.eventLog.dir       '${HOME}'/hdfs_dir/spark-events' >> $SPARK_HOME/conf/spark-defaults.conf
-    echo "spark.eventLog.compress  true" >> $SPARK_HOME/conf/spark-defaults.conf
-    echo 'spark.history.fs.logDirectory   '${HOME}'/hdfs_dir/spark-events' >> $SPARK_HOME/conf/spark-defaults.conf
-    echo "#StopSparkconf">> $SPARK_HOME/conf/spark-defaults.conf
-fi
-
 #setting spark log properties to display only errors
 cp $SPARK_HOME/conf/log4j.properties.template $SPARK_HOME/conf/log4j.properties
 sed -i 's/^log4j.rootCategory.*/log4j.rootCategory=ERROR, console/g' $SPARK_HOME/conf/log4j.properties
 
 ##Restoring hive-site.xml in conf dir under SPARK_HOME
-cp ${WORKDIR}/hive-site.xml ${SPARK_HOME}/conf
-
-for i in `echo $SERVERS |cut -d "=" -f2 | tr "%" "\n" | cut -d "," -f1`
+echo "Restoring config files spark-defaults.conf and hive-site.xml in spark folder"
+for i in `echo $SERVERS |tr "%" "\n"`
 do
-	scp $SPARK_HOME/conf/spark-defaults.conf @$i:$SPARK_HOME/conf | tee -a $log
 	scp $SPARK_HOME/conf/log4j.properties @$i:$SPARK_HOME/conf | tee -a $log
-	scp $SPARK_HOME/conf/hive-site.xml @$i:$SPARK_HOME/conf | tee -a $log
+	scp $WORKDIR/spark-defaults.conf @$i:$SPARK_HOME/conf | tee -a $log
+	scp $WORKDIR/hive-site.xml @$i:$SPARK_HOME/conf | tee -a $log
 done
 
 echo -e "Spark installation done..!!\n" | tee -a $log
